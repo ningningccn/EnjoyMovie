beef42af95e0230c4f3a7560f09790a1ffab0e25
ningningccn <44571910+ningningccn@users.noreply.github.com>
2020-12-22 02:02:12 +0800

    upload

--- src/store/cart.js
+++ src/store/cart.js
@@ -15,10 +15,8 @@ export default {
       const api = `${process.env.VUE_APP_API}/api/${process.env.VUE_APP_CUSTOMPATH}/cart`;
       context.commit('LOADING', true, { root: true });
       axios.get(api).then((response) => {
-        console.log(response.data.data);
         context.commit('CARTPRODUCTS', response.data.data);
         context.commit('LOADING', false, { root: true });
-        console.log('已取得cart資料');
       });
     },
     delCartItem(context, id) {
@@ -26,12 +24,10 @@ export default {
       context.commit('LOADING', true, { root: true });
       axios.delete(api).then((response) => {
         if (response.data.success) {
-          console.log(response.data.message);
           context.dispatch('getCart');
           context.commit('LOADING', false, { root: true });
         } else {
           context.commit('LOADING', false, { root: true });
-          console.log(response.data.message);
         }
       });
     },
@@ -42,7 +38,6 @@ export default {
         context.dispatch('delCartItem', oncecart[0].id);
         const tempProductid = oncecart[0].product_id;
         const tempNums = oncecart[0].qty + qty;
-        // console.log(tempNums, tempProductid);
         context.commit('LOADING', true, { root: true });
         const cart = {
           product_id: tempProductid,
--- src/store/index.js
+++ src/store/index.js
@@ -18,53 +18,13 @@ export default new Vuex.Store({
     },
     TEMPCATEGORY(state, payload) {
       state.tempCategory = payload;
-      // console.log(state.tempCategory);
     },
-    // PRODUCTS(state, payload) {
-    //   state.products = payload;
-    // },
-    // CATEGORIES(state, payload) {
-    //   const categories = new Set();
-    //   payload.forEach((item) => {
-    //     categories.add(item.category);
-    //   });
-    //   state.categories = Array.from(categories);
-    // },
-    // PAGINATION(state, payload) {
-    //   state.pagination = payload;
-    // },
   },
   // 行為
   actions: {
     updateLoading(context, status) {
       context.commit('LOADING', status);
     },
-  //   getProduct(context, page = 1) {
-  //     const api = `${process.env.VUE_APP_API}/api
-  // /${process.env.VUE_APP_CUSTOMPATH}/products?page=${page}`;
-  //     // console.log(page);
-  //     context.commit('LOADING', true); // 切換loading效果
-  //     axios.get(api).then((response) => {
-  //       context.commit('PRODUCTS', response.data.products);
-  //       context.commit('PAGINATION', response.data.pagination);
-  //       context.commit('CATEGORIES', response.data.products);
-  //       console.log('取得產品列表', response.data);
-  //       context.commit('LOADING', false);
-  //       // vm.getCategories();
-  //       // vm.$store.dispatch('updateLoading', false);
-  //     });
-  //   },
-  // },
-  // getters: {
-  //   categories(state) {
-  //     return state.categories;
-  //   },
-  //   products(state) {
-  //     return state.products;
-  //   },
-  //   pagination(state) {
-  //     return state.pagination;
-  //   },
   },
   getters: {},
   modules: {
